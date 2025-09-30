41bf6a4ce85f545eda57d6cd390ccd0e65c3ef55
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2025-09-30 20:00:45 -0300

    adicionando comentarios

--- trabalho-final-alterado/workers.py
+++ trabalho-final-alterado/workers.py
@@ -6,6 +6,15 @@ import os
 import random
 import numpy as np
 
+'''
+CPU: faz cálculos intensivos + detecta erros de instabilidade.
+
+RAM: aloca muita memória e acessa aleatoriamente.
+
+Disco: escreve/lê dados aleatórios em arquivo grande.
+
+'''
+
 
 def cpu_overclock_worker(stop_event: mp.Event, worker_id: int):
     """
@@ -30,16 +39,18 @@ def cpu_overclock_worker(stop_event: mp.Event, worker_id: int):
 def disk_worker(stop_event: mp.Event, file_path="disk_stress.tmp", block_size=4096):
     """Estressa Disco com escrita/leitura em posições aleatórias"""
     size = 1024 * 1024 * 1024  # 1 GB
+    # se o arquivo não existir, cria um arquivo grande
     if not os.path.exists(file_path):
         with open(file_path, "wb") as f:
             f.write(b"\0" * size)
-
+   # abre o arquivo para leitura/escrita binária
     with open(file_path, "r+b") as f:
         while not stop_event.is_set():
             pos = random.randint(0, size - block_size)
             data = os.urandom(block_size)
             f.seek(pos)
             f.write(data)
+            #força o flush para garantir escrita no disco
             f.flush()
             os.fsync(f.fileno())
 
